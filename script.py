08c58529b36c6e803ba8bcd84dd9fcbc815fb00b
John Garrett <jacgarrett18@gmail.com>
2019-12-25 13:20:59 -0500

    WIP

--- script.py
+++ script.py
@@ -1,13 +1,26 @@
 import piexif
 from os import listdir
 
-for image in listdir("./source_images"):
-    exif_dict = piexif.load("./source_images/"+image)
-    try:
-        print(f'\n--------{image}---------')
-        print(f'[MODEL]: {exif_dict["0th"][piexif.ImageIFD.Model]}')
-        print(f'[MAKE]: {exif_dict["0th"][piexif.ImageIFD.Make]}')
-        print(f'[ISO]: {exif_dict["Exif"][piexif.ExifIFD.ISOSpeed]}')
+def update_metadata(path, new_make, new_model):
+        exif_dict = piexif.load(path)
+        try:
+            print(f'\n--------{path}---------')
+            print(f'[MODEL]: {exif_dict["0th"][piexif.ImageIFD.Model]}')
+            print(f'[MAKE]: {exif_dict["0th"][piexif.ImageIFD.Make]}')
+            print(f'[ISO]: {exif_dict["Exif"][piexif.ExifIFD.ISOSpeed]}')
 
-    except:
-        print("Key does not exist")
+        except:
+            print("Key does not exist")
+        
+            exif_dict["0th"][piexif.ImageIFD.Model]}')
+            exif_dict["0th"][piexif.ImageIFD.Make]}')
+            exif_dict["Exif"][piexif.ExifIFD.ISOSpeed]}')
+
+def begin(dir):
+    make = input('Enter new make: ')
+    model = input('Enter new model: ')
+    iso = input('Ener film ISO: ')
+    for file in listdir(dir):
+        update_metadata(f'{dir}/{file}', make, model)
+if __name__=='__main__':
+    begin("./source_images")
