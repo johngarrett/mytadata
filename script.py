f5bf27f205e5ba7ae8902640fec240e23e275784
John Garrett <jacgarrett18@gmail.com>
2020-01-19 14:07:16 +0100

    rename files WIP

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
+#!/usr/local/bin/python3
 import piexif
 from os import listdir
 from PIL import Image
 
-def update_metadata(dir, image_name, make, model, iso):
+def update_metadata(dir, image_name, make, model, iso, new_image_name):
     original_dict = piexif.load(f'{dir}/{image_name}')
     x, y = original_dict["0th"][piexif.ImageIFD.XResolution], original_dict["0th"][piexif.ImageIFD.YResolution]
 
@@ -16,18 +17,18 @@ def update_metadata(dir, image_name, make, model, iso):
     first_ifd = zeroth_ifd
     exif_dict = {"0th":zeroth_ifd, "Exif":exif_ifd, "GPS":{}, "1st":first_ifd, "thumbnail":None}
     exif_bytes = piexif.dump(exif_dict)
-    piexif.insert(exif_bytes, f'./{dir}/{image_name}')
-    print(f'[{image_name}]: Sucess')
-
+    piexif.insert(exif_bytes, f'./{dir}/{new_image_name}')
+    print(f'[{image_name} -> {new_image_name}]: Sucess')
 
 def begin(dir):
-    make = input('Enter new make: ')
-    model = input('Enter new model: ')
-    iso = input('Ener film ISO: ')
-    for file in listdir(dir):
-       # try:
-        update_metadata(dir, file, make, model, iso)
-        #except:
-        #    print(f'[{file}]: Error updating metadata')
+    new_image_name = input('Enter a name for these photos: ')
+    make = input('Enter new make (e.g. Cannon): ')
+    model = input('Enter new model (e.g. AE-1): ')
+    iso = input('Ener film ISO (e.g. 400): ')
+    for (i, file) in enumerate(listdir(dir)):
+        try:
+            update_metadata(dir, file, make, model, iso, f'{new_image_name}{i}.jpeg')
+        except:
+            print(f'[{file}]: Error updating metadata')
 if __name__=='__main__':
-    begin("./source_images")
+    begin(input("Enter source image directory: "))
